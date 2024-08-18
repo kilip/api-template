737556341638f5fb11e789604074d17105de4ff7
Anthonius Munthi <me@itstoni.com>
2024-08-18 21:24:05 +0800

    chore(ci): configured based github action :rocket:

---
+++ src/Entity/Books.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of the api-template project.
+ *
+ * (c) Anthonius Munthi <me@itstoni.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ApiResource()]
+#[ORM\Entity()]
+class Books
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue()]
+    #[ORM\Column()]
+    public int $id;
+
+    #[ORM\Column(type: 'string')]
+    private string $title;
+
+    #[ORM\Column(type: 'string')]
+    private string $author;
+
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setAuthor(string $author): void
+    {
+        $this->author = $author;
+    }
+
+    public function getAuthor(): string
+    {
+        return $this->author;
+    }
+}
---
+++ tests/Functional/BooksTest.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+ * This file is part of the api-template project.
+ *
+ * (c) Anthonius Munthi <me@itstoni.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Functional;
+
+use App\Entity\Books;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectRepository;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class BooksTest extends KernelTestCase
+{
+    /**
+     * @var ObjectRepository<Books>
+     */
+    private ObjectRepository $repository;
+    private ObjectManager $manager;
+
+    protected function setUp(): void
+    {
+        static::bootKernel();
+        $this->manager = static::getContainer()
+            ->get('doctrine')
+            ->getManager();
+        $this->repository = $this->manager->getRepository(Books::class);
+    }
+
+    public function testCrud(): void
+    {
+        $repository = $this->repository;
+        $manager = $this->manager;
+
+        $this->ensureBookNotExists('test');
+
+        $book = new Books();
+        $book->setAuthor('Test');
+        $book->setTitle('Test Book');
+        $filter = ['title' => $book->getTitle()];
+
+        $manager->persist($book);
+        $manager->flush();
+        $this->assertInstanceOf(Books::class, $repository->findOneBy($filter));
+        $this->assertNotNull($book->id);
+        $this->assertSame('Test', $book->getAuthor());
+        $this->assertSame('Test Book', $book->getTitle());
+    }
+
+    private function ensureBookNotExists(string $title): void
+    {
+        $book = $this->repository->findOneBy(['title' => $title]);
+
+        if ($book instanceof Books) {
+            $this->manager->remove($book);
+            $this->manager->flush();
+        }
+    }
+}
