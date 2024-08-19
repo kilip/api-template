5e496cd3cd36879058b365de1fd8641af7c5f576
Anthonius Munthi <me@itstoni.com>
2024-08-19 08:04:08 +0800

    feat(core): added mercure and messenger

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
 ];
